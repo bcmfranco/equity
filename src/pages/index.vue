1e8dd30b47efb3218a271ed8e0873b7fc87448d8
bcmfranco <bruno@codev.com.ar>
2024-04-19 11:24:00 -0300

    Delete death code

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -25,27 +25,27 @@
       <div id="sentence">
         <div id="total_wrapper">
           <div class="type_totals">
-            <div class="type_totals_wrappers" id="abel_total" v-if="this.newResposibles.player1[1] > 0">
+            <div class="type_totals_wrappers" v-if="this.newResposibles.player1[1] > 0">
               <label for="total" style="font-weight: normal;">{{ this.newResposibles["player1"][0] }}</label>
               <input class="total_input" :value='this.newResposibles["player1"][1]' disabled />
             </div>
 
-            <div class="type_totals_wrappers" id="abel_total" v-if="this.newResposibles.player2[1] > 0">
+            <div class="type_totals_wrappers" v-if="this.newResposibles.player2[1] > 0">
               <label for="total" style="font-weight: normal;">{{ this.newResposibles["player2"][0] }}</label>
               <input class="total_input" :value='this.newResposibles["player2"][1]' disabled />
             </div>
 
-            <div class="type_totals_wrappers" id="abel_total" v-if="this.newResposibles.player3[1] > 0">
+            <div class="type_totals_wrappers" v-if="this.newResposibles.player3[1] > 0">
               <label for="total" style="font-weight: normal;">{{ this.newResposibles["player3"][0] }}</label>
               <input class="total_input" :value='this.newResposibles["player3"][1]' disabled />
             </div>
 
-            <div class="type_totals_wrappers" id="abel_total" v-if="this.newResposibles.player4[1] > 0">
+            <div class="type_totals_wrappers" v-if="this.newResposibles.player4[1] > 0">
               <label for="total" style="font-weight: normal;">{{ this.newResposibles["player4"][0] }}</label>
               <input class="total_input" :value='this.newResposibles["player4"][1]' disabled />
             </div>
 
-            <div class="type_totals_wrappers" id="abel_total" v-if="this.newResposibles.player5[1] > 0">
+            <div class="type_totals_wrappers" v-if="this.newResposibles.player5[1] > 0">
               <label for="total" style="font-weight: normal;">{{ this.newResposibles["player5"][0] }}</label>
               <input class="total_input" :value='this.newResposibles["player5"][1]' disabled />
             </div>
@@ -66,9 +66,6 @@
 
     <div ref="wizzard2" class="wizzard" wizzard="2" style="display: none;">
     
-      poniendo estaba la gansa
-
-
         <div class="debt_totals">
             <div class="type_totals_wrappers" id="abel_total" v-if="this.newResposibles.player1[2] > 0">
               <span>{{ this.newResposibles["player1"][0] }}</span>
@@ -121,30 +118,6 @@ export default {
   data() {
     return {
       newItem: null,
-      items: [],
-      itemsBonzo: [],
-      itemsCarlos: [],
-      itemsDaniel: [],
-      itemsEnzo: [],
-      total: 0,
-      totalBonzo: 0,
-      totalCarlos: 0,
-      totalDaniel: 0,
-      totalEnzo: 0,
-      totalMax: 0,
-      totalPlayerSpendt: 0,
-      abelPercentage: 20,
-      bonzoPercentage: 20,
-      carlosPercentage: 20,
-      danielPercentage: 20,
-      enzoPercentage: 20,
-      wsp_content: "Vacío",
-      sortedItems: [],
-      phoneNumber: "+543413690080",
-      abel: 0,
-      bonzo: 0,
-      newItemValue: null,
-      newItemResponsible: null,
       newResposibles: {"player1": ["player1", null], "player2": ["player2", null], "player3": ["player3", null], "player4": ["player4", null], "player5": ["player5", null]},
       lastPlayerEdited: 0,
       spendingObj: {},
@@ -153,55 +126,8 @@ export default {
   },
   mounted() {
 
-    // Read token and write items
-    var token = window.location.search.substring(1);
-    if(token){
-      var parts = token.split("zzz");
-      var allItems = parts.map(part => {
-        var [id, value, type] = part.split(",");
-        return { id: parseInt(id), value: parseInt(value), type: type === "f" ? "abel" : "bonzo" };
-      });
-
-      allItems.forEach(item => {
-        if(item.type === "abel") {
-          this.items.push(item);
-        } else if (item.type === "bonzo") {
-          this.itemsBonzo.push(item);
-        } else if (item.type === "carlos") {
-          this.itemsCarlos.push(item);
-        } else if (item.type === "daniel") {
-          this.itemsDaniel.push(item);
-        } else {
-          this.itemsEnzo.push(item);
-        }
-      });
-
-      this.abelSum();
-      this.bonzoSum();
-      this.carlosSum();
-      this.danielSum();
-      this.enzoSum();
-      this.maxSum();
-
-    }
-    ////////////////
   },
   methods: {
-    abelSum(){
-      return this.total = this.items.reduce((total, item) => total + parseInt(item.value), 0);
-    },
-    bonzoSum(){
-      return this.totalBonzo = this.itemsBonzo.reduce((total, item) => total + parseInt(item.value), 0);
-    },
-    carlosSum(){
-      return this.totalBonzo = this.itemsCarlos.reduce((total, item) => total + parseInt(item.value), 0);
-    },
-    danielSum(){
-      return this.totalBonzo = this.itemsDaniel.reduce((total, item) => total + parseInt(item.value), 0);
-    },
-    enzoSum(){
-      return this.totalBonzo = this.itemsEnzo.reduce((total, item) => total + parseInt(item.value), 0);
-    },
     totalSum(){ // Devuelve la suma de todos los gastos de todos los players
 
       this.totalPlayerSpendt = 0;
@@ -278,191 +204,26 @@ export default {
         this.$refs.wizzard1.style.display = 'none';
         this.$refs.wizzard2.style.display = 'block';
 
-        this.listPayers();
         return this.newResposibles;
       }
     },
 
-    back(){
-
-        // Muestro el wizzard 1 y oculto el 2
+    back(){ // Muestro el wizzard 1 y oculto el 2
         this.$refs.wizzard1.style.display = 'block';
         this.$refs.wizzard2.style.display = 'none';
-
     },
 
-    listPayers(){
-
+    listPayers(){ // Listo los players que tienen deuda
       for (let key in this.newResposibles) {
         if(this.newResposibles[key][2] > 0){
           console.log(this.newResposibles[key]);
         }
       }
-
     },
 
-    addItem(cateogry) {
-
-      if(cateogry == 1){ // abel
-        if (this.newItem !== null) {
-          this.items.push({ id: Date.now(), value: this.newItem, type: "abel" });
-          this.newItem = null;
-          this.abelSum();
-
-        }
-      } else if (cateogry == 2) { // bonzo
-        if (this.newItem !== null) {
-          this.itemsBonzo.push({ id: Date.now(), value: this.newItem, type: "bonzo" });
-          this.newItem = null;
-          this.bonzoSum();
-        }
-      } else if (cateogry == 3) { // carlos
-        if (this.newItem !== null) {
-          this.itemsCarlos.push({ id: Date.now(), value: this.newItem, type: "carlos" });
-          this.newItem = null;
-          this.carlosSum();
-        }
-      } else if (cateogry == 4) { // daniel
-        if (this.newItem !== null) {
-          this.itemsDaniel.push({ id: Date.now(), value: this.newItem, type: "daniel" });
-          this.newItem = null;
-          this.danielSum();
-        }
-      } else { // enzo
-        if (this.newItem !== null) {
-          this.itemsEnzo.push({ id: Date.now(), value: this.newItem, type: "enzo" });
-          this.newItem = null;
-          this.enzoSum();
-        }
-      }
-
-      this.maxSum();
-    },
-    deleteItem(id) {
-
-      let index = this.items.findIndex(item => item.id === id);
-      if (index !== -1) {
-        this.total -= parseInt(this.items[index].value);
-        this.totalMax -= parseInt(this.items[index].value);
-        this.items.splice(index, 1);
-      }
-
-      index = this.itemsBonzo.findIndex(item => item.id === id);
-      if (index !== -1) {
-        this.totalBonzo -= parseInt(this.itemsBonzo[index].value);
-        this.totalMax -= parseInt(this.itemsBonzo[index].value);
-        this.itemsBonzo.splice(index, 1);
-      }
-
-      index = this.itemsCarlos.findIndex(item => item.id === id);
-      if (index !== -1) {
-        this.totalCarlos -= parseInt(this.itemsCarlos[index].value);
-        this.totalMax -= parseInt(this.itemsCarlos[index].value);
-        this.itemsCarlos.splice(index, 1);
-      }
-
-      index = this.itemsDaniel.findIndex(item => item.id === id);
-      if (index !== -1) {
-        this.totalDaniel -= parseInt(this.itemsDaniel[index].value);
-        this.totalMax -= parseInt(this.itemsDaniel[index].value);
-        this.itemsDaniel.splice(index, 1);
-      }
-
-      index = this.itemsEnzo.findIndex(item => item.id === id);
-      if (index !== -1) {
-        this.totalBonzo -= parseInt(this.itemsEnzo[index].value);
-        this.totalMax -= parseInt(this.itemsEnzo[index].value);
-        this.itemsEnzo.splice(index, 1);
-      }
-     
-      this.abelSum();
-      this.bonzoSum();
-      this.carlosSum();
-      this.danielSum();
-      this.enzoSum();
-    },
-    prepareWhatsAppMessage() {
-      // Genero token
-      if(this.items[0] || this.itemsBonzo[0]){
-        var allItems = [...this.items, ...this.itemsBonzo].sort((a, b) => a.id - b.id);
-        
-        var itemJoined = allItems.map(item => {
-          return item.id + "," + item.value + "," + (item.type === "abel" ? "f" : "v");
-        }).join("zzz");
-      }
-
-      // Obtener el número de teléfono del input
-      this.phone_number = this.phoneNumber;
-
-      // Paso el token como contenido de wsp
-      this.wsp_content = itemJoined;
-      window.location.href = 'https://api.whatsapp.com/send?phone=' + this.phone_number +'&text=' + window.location.href + "?" + encodeURIComponent(this.wsp_content);
-    },
-    calculateDebt() {
-      if (this.total !== null && this.totalBonzo !== null && this.totalCarlos !== null && this.totalDaniel !== null && this.totalEnzo !== null) {
-        const totalGasto = this.total + this.totalBonzo + this.totalCarlos + this.totalDaniel + this.totalEnzo;
-        const pagoPorPersona = totalGasto / 5;
-
-        let debt = {};
-        if (this.total < pagoPorPersona) {
-          debt.abel = (pagoPorPersona - this.total).toFixed(0);
-        } else {
-          debt.bonzo = (this.total - pagoPorPersona).toFixed(0);
-        }
-
-        if (this.totalBonzo < pagoPorPersona) {
-          debt.bonzo = (pagoPorPersona - this.totalBonzo).toFixed(0);
-        } else {
-          debt.carlos = (this.totalBonzo - pagoPorPersona).toFixed(0);
-        }
-
-        if (this.totalCarlos < pagoPorPersona) {
-          debt.carlos = (pagoPorPersona - this.totalCarlos).toFixed(0);
-        } else {
-          debt.daniel = (this.totalCarlos - pagoPorPersona).toFixed(0);
-        }
-
-        if (this.totalDaniel < pagoPorPersona) {
-          debt.daniel = (pagoPorPersona - this.totalDaniel).toFixed(0);
-        } else {
-          debt.enzo = (this.totalDaniel - pagoPorPersona).toFixed(0);
-        }
-
-        if (this.totalEnzo < pagoPorPersona) {
-          debt.enzo = (pagoPorPersona - this.totalEnzo).toFixed(0);
-        } else {
-          debt.abel = (this.totalEnzo - pagoPorPersona).toFixed(0);
-        }
-
-        return debt;
-      }
-      return null;
-    }
   },
   computed: {
-    sortedItems() {
-      var allItems = [...this.items, ...this.itemsBonzo, ...this.itemsCarlos, ...this.itemsDaniel, ...this.itemsEnzo];
-      return allItems.sort((a, b) => a.id - b.id);
-    },
-    totalAbelPercentage() {
-      return `${this.newResposibles["player1"][1]}`
-    },
-    totalBonzoPercentage() {
-      this.bonzoPercentage = Math.round(100 / this.totalMax * this.totalBonzo);
-      return `${this.totalBonzo}`;
-    },
-    totalCarlosPercentage() {
-      this.carlosPercentage = Math.round(100 / this.totalMax * this.totalCarlos);
-      return `${this.totalCarlos}`;
-    },
-    totalDanielPercentage() {
-      this.danielPercentage = Math.round(100 / this.totalMax * this.totalDaniel);
-      return `${this.totalDaniel}`;
-    },
-    totalEnzoPercentage() {
-      this.enzoPercentage = Math.round(100 / this.totalMax * this.totalEnzo);
-      return `${this.totalEnzo}`;
-    }
+
   }
 };
 </script>
